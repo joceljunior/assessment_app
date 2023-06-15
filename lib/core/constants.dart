b0eba201863d1824383cf05edd0f970aaaa5e4ed
Jocel <jocel.junior.silva@gmail.com>
2023-06-15 10:55:15 -0300

    alter route

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const baseurl = "https://assessment-api.herokuapp.com/";
+  static const baseurl = "http://13.59.105.48:442/";
   // static const baseurl = "http://192.168.3.30:3000/";
 }
